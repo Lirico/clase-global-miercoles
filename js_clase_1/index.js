8c563c5d61ecdea1dc610da12676ff8e2733d3ef
lirico <dominguezmatiasadrian@gmail.com>
2025-01-12 11:01:54 -0300

    clase js 1 y 2

---
+++ js_clase_1/index.js
@@ -0,0 +1,25 @@
+
+
+
+// Tipos de datos
+
+13  // number
+
+"salu2" // string -> cadena de texto
+
+true, false // booleano -> algebra de boole
+
+// console.log(13 + 14) // Suma
+// console.log("Hola" + " " + "Pepe") // Concatenacion
+// console.log(13 + "Hola")
+
+// Variable -> Guardar un valor para poder reutilizarlo
+
+
+
+var uno = 1 // Este valor, muere
+
+var uno = 2
+
+console.log(uno)
+
---
+++ js_clase_2/index.js
@@ -0,0 +1,30 @@
+
+
+
+
+/* OPERACIONES ARTIMETICAS */
+// Suma +
+// Resta -
+// Mutiplicacion *
+// Division /
+// Modulo %
+// Incrementadores ++
+// Decrementadoras --
+
+/* OPERACIONES COMPARATIVAS */
+// Mayor que >
+// Menor que <
+// Mayor o igual que > =
+// Menor o igual que < =
+// Igualdad estricta = = =
+// Igualdad no estricta = =
+// Desigualdad estricta ! = =
+// Desigualdad no estricta ! =
+
+/* OPERACIONES LOGICAS */
+// AND -> Y -> &&
+// OR -> O -> ||
+// NOT -> negacion -> !
+
+
+
