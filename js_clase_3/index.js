8b0b331af3677ae9e0baadec21d2ecbf19db2b48
lirico <dominguezmatiasadrian@gmail.com>
2025-01-15 23:00:35 -0300

    clase 3 js

---
+++ js_clase_3/index.js
@@ -0,0 +1,84 @@
+
+
+// Estructuras de control de flujo
+
+// Concepto de Algoritmo
+
+// Un secuencia de instrucciones que se ejecutan en orden
+
+// var itRains = false
+
+// var itsHot = true
+
+// console.log("INSTRUCCION 1")
+// console.log("INSTRUCCION 2")
+
+// if(itRains){
+//     console.log("INSTRUCCION 3")
+// } 
+// if(itsHot){
+//     console.log("INSTRUCCION 4")
+// }
+
+// console.log("INSTRUCCION 5")
+
+
+// var numero = 10
+
+
+// if(numero > 10){
+//     console.log("Numero es mayor que 10")
+// } else if(numero < 10) {
+//     console.log("Numero es menor que 10")
+// } else {
+//     console.log("Numero es igual a 10")
+// }
+
+
+// SWITCH CASE
+// var producto = "pepe";
+
+// switch (producto) {
+//     case 1: 
+//         console.log("Agua")
+//         break;
+//     case 2: 
+//         console.log("Te")
+//         break;
+//     case 3:
+//         console.log("Cafe")
+//         break;
+//     case 4:         
+//         console.log("Energizante") 
+//         break;
+//     default:
+//         console.log("CD de Maluma")
+// }
+
+
+// BUCLES O CICLOS (LOOPS)
+
+// while -> mientras
+
+var contador = 0;
+
+while(contador < 10){
+    console.log(contador + " while 1")
+    contador++
+}
+
+while(contador < 10){
+    console.log(contador + " while 2")
+    contador++
+}
+
+// do while -> hacer mientras
+// do {
+//     console.log(contador + " do while")
+//     contador++
+// } while (contador < 10);
+
+// do {
+//     console.log(contador + " do while")
+//     contador++
+// } while (contador < 10);
